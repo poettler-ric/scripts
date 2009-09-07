4feef50566c2f517952f365505fc9f07cb9c6613
Richard Poettler <richard.poettler@gmail.com>
2009-09-07 08:07:44 +0200

    wpLeecher.pl: added leeching of all wallpapers of one babe
    this is done with the -g switch

--- wpLeecher.pl
+++ wpLeecher.pl
@@ -16,7 +16,7 @@ $Getopt::Std::STANDARD_HELP_VERSION = 1;
 # version of the script
 our $VERSION = '$Revision: 1.6 $' =~ /([\d.]+)/ && $1;
 
-getopts("abld:p:o:s:n", \%opts);
+getopts("abld:g:p:o:s:n", \%opts);
 
 # default file containing the list of already downloaded files
 my $fileLeeched = $ENV{"HOME"} . "/.leeched";
@@ -27,9 +27,11 @@ my $dirImages = $opts{"o"} || $ENV{"HOME"} . "/wp";
 # url to the skins.be index page
 my $urlRoot = "http://www.skins.be/";
 # pattern for the pages listing new images
-my $urlNewestList = "http://www.skins.be/page/\@page/";
+my $urlNewestPattern = "http://www.skins.be/page/\@page/";
 # pattern for the image url
 my $urlImage = "http://wallpapers.skins.be/\@name/\@name-\@resolution-\@id.jpg";
+# pattern for the pages of one specific babe
+my $urlBabePagePattern = "http://www.skins.be/\@name/wallpapers/page/\@page/";
 
 # precompiled regexp to geht some data out of a imagelink ($1 = name, $2 = id, # $3 = resolution)
 my $nodeDataRegexp = qr/http:\/\/wallpaper.skins.be\/([\w-]+)\/(\d+)\/(\d+x\d+)\//;
@@ -44,13 +46,14 @@ sub HELP_MESSAGE {
 	my ($fh) = @_;
 	print $fh <<eof
 usage:
-$0 [-a] [-b] [-l] [-d <file>] [-p <number>] [-o <directory>] [-s <number>] [-n]
+$0 [-a] [-b] [-l] [-d <file>] [-g <name>] [-p <number>] [-o <directory>] [-s <number>] [-n]
 	-a stop parsing pages automatically, if a page was parsed with no new pictures
 	-b scanning is started from the last page to the newest one
 	-l lists all downloaed images to stdout and exits (the 
 	   resulting file can be used for the -d)
 	-d takes a file, which contains a list of relative filenames 
 	   which should be threaded as already downloaded
+	-g name of the babe of which to retrieve all pictures
 	-p specifies the number of pages which should be scanned
 	-o species the output directory for the files (of from where 
 	   the files should be listed)
@@ -88,7 +91,8 @@ sub getNodeData {
 }
 
 sub getLastPage {
-	my $response = $ua->get($urlRoot);
+	my $url = shift or croak "no url given";
+	my $response = $ua->get($url);
 	croak $response->message unless $response->is_success;
 	# parsing the content
 	my $tree = HTML::TreeBuilder->new_from_content($response->content);
@@ -167,9 +171,10 @@ sub wanted {
 	print basename($File::Find::dir), "/", $_, "\n" if (-f $File::Find::name);
 }
 
-sub doNewestPage {
-	my ($page) = @_;
-	my $url = $urlNewestList;
+sub doPage {
+	my $page = shift or croak "no pageid given";
+	my $url = shift or $urlNewestPattern;
+
 	$url =~ s/\@page/$page/g;
 	my $gotNewPicture = 0;
 
@@ -200,11 +205,28 @@ if ($opts{"l"}) { # list downloaded files
 loadDownloaded($opts{"d"}) if ($opts{"d"}); # load a file with a list of already downloaded files
 loadDownloaded($fileLeeched) if (-f $fileLeeched && !$opts{"n"}); # loading the default file
 
-my @pages = (1 .. getLastPage());
+my $urlPattern = "";
+my $pageCount = 0;
+
+if ($opts{"g"}) {
+	my $name = $opts{"g"};
+	$name = lc($name);
+	$name =~ s/ /-/g;
+
+	$urlPattern = $urlBabePagePattern;
+	$urlPattern =~ s/\@name/$name/g;
+	(my $firstPage = $urlPattern) =~ s/\@page/1/g;
+	$pageCount = getLastPage($firstPage);
+} else {
+	$urlPattern = $urlNewestPattern;
+	$pageCount = getLastPage($urlRoot);
+}
+
+my @pages = (1 .. $pageCount);
 @pages = reverse @pages if ($opts{"b"}); # iterate the new pages in reverse order
 @pages = @pages[$opts{"s"} .. $#pages] if ($opts{"s"}); # skip some pages
 @pages = @pages[0 .. ($opts{"p"} - 1)] if ($opts{"p"}); # set number of pages to process
 
 foreach my $page (@pages) {
-	doNewestPage($page) || $opts{"a"} && exit 0;
+	doPage($page, $urlPattern) || $opts{"a"} && exit 0;
 }
