2cb2a39cb2487a84e454f505fb525cbba095f401
Richard Poettler <richard.poettler@gmail.com>
2015-10-14 15:29:48 +0200

    added script to summarize the office hours

---
+++ officehours.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+from datetime import datetime, timedelta
+
+__DATE_FORMAT = "%Y%m%d%H%M"
+__HOUR_FORMAT = "%H:%M"
+__START_TIME = "197001010800"
+__SIX_HOURS = timedelta(0, 0, 0, 0, 0, 6)
+__THIRDY_MINUTES = timedelta(0, 0, 0, 0, 30)
+
+def decimal_delta(delta):
+    return delta.total_seconds() / 3600
+
+def parse_weeks(filename):
+    weeks = {}
+    with open(filename) as f:
+        for line in f:
+            parts = line.split()
+
+            from_time = datetime.strptime(parts[0] + parts[1], __DATE_FORMAT)
+            to_time = datetime.strptime(parts[0] + parts[2], __DATE_FORMAT)
+            week_number = from_time.isocalendar()[1]
+
+            week = weeks.get(week_number, {
+                'duration': timedelta(),
+                'comments': []})
+            week['duration'] = week['duration'] + (to_time - from_time)
+            week['comments'].append(" ".join(parts[3:]))
+            weeks[week_number] = week
+
+    return weeks
+
+def print_weeks(weeks):
+    start = datetime.strptime(__START_TIME, __DATE_FORMAT)
+    for week_number, week in weeks.iteritems():
+        print("week %d" % week_number)
+        duration = week['duration']
+        print("duration: %s (%.2f)" % (duration, decimal_delta(duration)))
+        end = start + duration
+        if duration > __SIX_HOURS:
+            end = end + __THIRDY_MINUTES
+            print("!! BOOK BREAK !!")
+        print("booking: %s - %s"
+            % (start.strftime(__HOUR_FORMAT), end.strftime(__HOUR_FORMAT)))
+        print("comments:")
+        for comment in week['comments']:
+            print(comment)
+        print("")
+
+if __name__ == '__main__':
+    print_weeks(parse_weeks("times.txt"))
