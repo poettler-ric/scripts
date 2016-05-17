23005927c08353a7bcc77d3203d80d7631187f90
Richard Poettler <richard.poettler@gmail.com>
2016-05-17 11:02:31 +0200

    renamed variables and documented them for rpmfusioin

--- sync.fedora.sh
+++ sync.fedora.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-# define RELEASES, BASEARCH, LOCAL_ROOT and MIRROR_ROOT in ~/.sync.fedora.sh
+# following variables are possible to set in  ~/.sync.fedora.sh
+#
+# BASEARCH ... architectures to sync
+#
+# FEDORA_RELEASES ... fedora releases to sync e.g. "1 2"
+# FEDORA_LOCAL ... local root directory for fedora repositories
+# FEDORA_MIRROR ... remote root directory for fedora repositories
+#
+# RPMFUSION_RELEASES ... rpmfusion releases to sync e.g. "1 2"
+# RPMFUSION_LOCAL ... local root directory for rpmfusion repositories
+# RPMFUSION_MIRROR ... remote root directory for rpmfusion repositories
+
 
 readonly RC_FILE=~/.sync.fedora.sh
 
@@ -9,13 +20,13 @@ then
     . "$RC_FILE"
 fi
 
-for RELEASE_VER in $RELEASES
+for RELEASE_VER in $FEDORA_RELEASES
 do
-    RELEASE_URL="rsync://$MIRROR_ROOT/releases/$RELEASE_VER/Everything/$BASEARCH/os/"
-    RELEASE_DIR="${LOCAL_ROOT}/releases/$RELEASE_VER/Everything/$BASEARCH/os/"
+    RELEASE_URL="rsync://$FEDORA_MIRROR/releases/$RELEASE_VER/Everything/$BASEARCH/os/"
+    RELEASE_DIR="${FEDORA_LOCAL}/releases/$RELEASE_VER/Everything/$BASEARCH/os/"
 
-    UPDATE_URL="rsync://$MIRROR_ROOT/updates/$RELEASE_VER/$BASEARCH/"
-    UPDATE_DIR="${LOCAL_ROOT}/updates/$RELEASE_VER/$BASEARCH/"
+    UPDATE_URL="rsync://$FEDORA_MIRROR/updates/$RELEASE_VER/$BASEARCH/"
+    UPDATE_DIR="${FEDORA_LOCAL}/updates/$RELEASE_VER/$BASEARCH/"
 
     mkdir -p "$RELEASE_DIR"
     mkdir -p "$UPDATE_DIR"
